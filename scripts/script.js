351b36d413627873d2297681e5e2745cdd36131d
Animesh-X <animeshkumar0715@gmail.com>
2023-10-27 09:21:43 +0530

    Changes made to script file

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,18 @@ function reload(){
 }
 
 async function fetchNews(query){
-    const res=await fetch(`${url}${query}&apiKey=${API_KEY}`);
-    const data=await res.json();
-    bindData(data.articles);
+    try {
+        const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await res.json();
+        bindData(data.articles);
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
 }
+
 
 function bindData(articles){
     const cardsContainer=document.getElementById('cards-container');
